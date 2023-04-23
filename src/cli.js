a3f1686ce1b1e8936d1f330c23ab8fb652a12098
DieWerkself <redead29@gmail.com>
2023-04-23 16:13:34 +0300

    refactoring cli

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,5 @@ import readlineSync from 'readline-sync';
 export default () => {
   console.log('Welcome to the Brain Games!');
   let userName = readlineSync.question('May I have your name? ');
-  if (!userName.trim()) userName = 'John Doe';
   console.log(`Hello, ${userName}!`);
-  return userName;
 };
